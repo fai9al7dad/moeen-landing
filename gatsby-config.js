9d9ba7d312c4f81996c1c7d24984e4357fc80b4f
fai9al7dad@gmail.com <fai9al7dad@gmail.com>
2022-08-13 16:03:20 +0300

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `تطبيق معين`,
+        short_name: `moeen`,
         start_url: `/`,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
