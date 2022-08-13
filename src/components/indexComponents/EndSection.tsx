2ec53c2ed3e66844a3302d11ff8c4e2ad5331fff
fai9al7dad@gmail.com <fai9al7dad@gmail.com>
2022-08-13 20:35:19 +0300

    fixed end section logo

--- src/components/indexComponents/EndSection.tsx
+++ src/components/indexComponents/EndSection.tsx
@@ -8,15 +8,15 @@ export default function EndSection() {
     <div className="min-h-screen flex flex-col items-center justify-center lg:px-10">
       <div className=" lg:w-2/4 flex flex-col items-center justify-center">
         <div className="px-5">
-          <img src={logo} className="rounded-lg shadow-2xl w-60 " />
+          <img src={logo} className="rounded-lg shadow-2xl w-48 lg:w-60 " />
         </div>
 
-        <div className="mt-10 text-6xl lg:text-8xl bold relative text-center">
+        <div className="mt-10 text-5xl lg:text-8xl bold relative text-center">
           تطبيق معين
           <div className="w-7 lg:w-12 h-7 lg:h-12 rounded-full bg-warning absolute bottom-0 left-28 lg:left-36 -z-10" />
           <div className="w-7 lg:w-12 h-7 lg:h-12 rounded-full bg-mistake absolute top-5 right-0 lg:right-20 -z-10" />
         </div>
-        <div className="mt-10 text-2xl  lg:text-3xl text-center px-5">
+        <div className="mt-10 text-xl  lg:text-3xl text-center px-5">
           مصحف الحفظ والمراجعة
         </div>
       </div>
